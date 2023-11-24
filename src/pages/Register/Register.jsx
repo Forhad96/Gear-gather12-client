08ac95988dd3b5c89a37b04dae8a529eba907278
Forhad96 <forhadairdrop@gmail.com>
2023-11-25 00:34:44 +0600

    create register user page and routes

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,85 @@
+import { Link } from "react-router-dom";
+import SocialLogin from "../../shared/SocialLogin/SocialLogin";
+import Logo from "../../shared/Logo/Logo";
+
 const Register = () => {
-    return (
-        <div>
-            <p>HELLO I am Register</p>
+  const handleRegister = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <main className="w-full h-screen flex flex-col items-center justify-center px-4">
+      <div className="max-w-sm w-full text-gray-600 space-y-5">
+        <div className="text-center pb-8">
+          <Logo></Logo>
+          <div className="mt-5">
+            <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">
+              Log in to your account
+            </h3>
+          </div>
         </div>
-    );
+        <form onSubmit={() => handleRegister} className="space-y-5">
+          <div>
+            <label className="font-medium">Name</label>
+            <input
+              type="text"
+              name="name"
+              required
+              className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            />
+          </div>
+          <div>
+            <label className="font-medium">Email</label>
+            <input
+              type="email"
+              name="email"
+              required
+              className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            />
+          </div>
+          <div>
+            <label className="font-medium">Password</label>
+            <input
+              type="password"
+              name="password"
+              required
+              className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            />
+          </div>
+          <div>
+            <label className="font-medium">Photo Url</label>
+            <input
+              type="text"
+              name="photo"
+              required
+              className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            />
+          </div>
+          <div className="flex items-center justify-between text-sm">
+            <div className="flex items-center gap-x-3">
+              <input
+                type="checkbox"
+                id="remember-me-checkbox"
+                className="checkbox-item peer hidden"
+              />
+              <span>Remember me</span>
+            </div>
+          </div>
+          <button className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150">
+            Register
+          </button>
+        </form>
+        <SocialLogin></SocialLogin>
+        <p className="text-center">
+          Already have an account?{" "}
+          <Link
+            to="/register"
+            className="font-medium text-indigo-600 hover:text-indigo-500"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+    </main>
+  );
 };
 export default Register;
