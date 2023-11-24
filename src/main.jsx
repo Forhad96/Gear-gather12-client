eb6060628865081fcc1c20ba41871cf0940e0a06
Forhad96 <forhadairdrop@gmail.com>
2023-11-25 01:35:59 +0600

    created auth provider for authenticaion

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import route from './router/route'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import route from "./router/route";
 
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./providers/AuthProvider";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-   
-    <RouterProvider router={route}></RouterProvider>
+      <AuthProvider>
+        <RouterProvider router={route}></RouterProvider>
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
